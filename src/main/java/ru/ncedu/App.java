a1df42b8536fc61b49c6b69cf0588264d7a2757b
Alexander Ionov <orlicus@gmail.com>
2016-11-07 14:51:09 +0400

    Initial commit

---
+++ src/main/java/ru/ncedu/App.java
@@ -0,0 +1,13 @@
+package ru.ncedu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
