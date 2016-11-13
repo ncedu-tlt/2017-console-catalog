85138e201069fbaade0603945db61de555dcf8fe
Anton Kuzmin <Garthdex@gmail.com>
2016-11-13 23:16:33 +0400

    add Characteristic class and CharacteristicRepository class

---
+++ src/main/java/ru/ncedu/menu/models/Characteristic.java
@@ -0,0 +1,57 @@
+package ru.ncedu.menu.models;
+
+import java.io.Serializable;
+
+public class Characteristic implements Serializable {
+    private long id;
+    private String name;
+    private long categoryId;
+    private long groupId;
+
+    public Characteristic() {
+    }
+
+    public Characteristic(long id, String name, long categoryId, long groupId) {
+        this.id = id;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.groupId = groupId;
+    }
+
+    public Characteristic(Characteristic characteristic) {
+        this(characteristic.getId(), characteristic.getName(),
+                characteristic.getCategoryId(), characteristic.getGroupId());
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+}
---
+++ src/main/java/ru/ncedu/menu/repositories/CharacteristicRepository.java
@@ -0,0 +1,98 @@
+package ru.ncedu.menu.repositories;
+
+import ru.ncedu.menu.models.Characteristic;
+import ru.ncedu.menu.utils.JSONUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharacteristicRepository implements Repository<Characteristic> {
+
+    private static final String FILE_NAME = "characteristics.json";
+    private static final long START_ID = 1;
+
+    private static CharacteristicRepository instance;
+
+    private List<Characteristic> characteristics;
+
+    private CharacteristicRepository() {}
+
+    public static synchronized CharacteristicRepository getInstance() {
+        if (instance == null) {
+            instance = new CharacteristicRepository();
+            instance.load();
+        }
+        return instance;
+    }
+
+    @Override
+    public List<Characteristic> get() {
+        return characteristics;
+    }
+
+    public Characteristic get(long id) {
+        for (Characteristic characteristic : characteristics) {
+            if (characteristic.getId() == id) return characteristic;
+        }
+        return null;
+    }
+
+    @Override
+    public Characteristic add(Characteristic object) {
+        if (object == null) return null;
+
+        Characteristic characteristic = new Characteristic(object);
+        characteristic.setId(generateId());
+
+        characteristics.add(characteristic);
+        return characteristic;
+    }
+
+    @Override
+    public Characteristic update(Characteristic object) {
+        if (object == null) return null;
+
+        Characteristic characteristic = new Characteristic(object);
+        remove(characteristic);
+
+        characteristics.add(characteristic);
+        return characteristic;
+    }
+
+    @Override
+    public void remove(Characteristic object) {
+        if (object == null) return;
+
+        Characteristic characteristic = get(object.getId());
+        characteristics.remove(characteristic);
+    }
+
+    @Override
+    public void save() {
+        try {
+            JSONUtils.writeToFile(FILE_NAME, characteristics);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void load() {
+        try {
+            characteristics = JSONUtils.readListFromFile(FILE_NAME, Characteristic.class);
+        } catch (IOException e) {
+            characteristics = new ArrayList<>();
+            e.printStackTrace();
+        }
+    }
+
+    private long generateId() {
+        long id = START_ID;
+        for (Characteristic charasteristic : characteristics) {
+            id = Math.max(id, charasteristic.getId());
+        }
+
+        return ++id;
+    }
+}
