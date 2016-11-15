788c9cbfce3b968adb8c550ecda694e6c60623f4
Anton Kuzmin <Garthdex@gmail.com>
2016-11-16 02:15:32 +0400

    finish characteristic menu command

--- src/main/java/ru/ncedu/menu/commands/characteristic/CharacteristicMenuCommand.java
+++ src/main/java/ru/ncedu/menu/commands/characteristic/CharacteristicMenuCommand.java
@@ -1,6 +1,10 @@
 package ru.ncedu.menu.commands.characteristic;
 
 import ru.ncedu.menu.commands.Command;
+import ru.ncedu.menu.commands.MainMenuCommand;
+import ru.ncedu.menu.utils.MenuUtils;
+
+import java.util.Scanner;
 
 
 public class CharacteristicMenuCommand implements Command {
@@ -19,6 +23,32 @@ public class CharacteristicMenuCommand implements Command {
 
     @Override
     public Command execute() {
-        return null;
+        MenuUtils.printSeparator();
+        MenuUtils.printOption("1", "View characteristics");
+        MenuUtils.printOption("2", "Add characteristic");
+        MenuUtils.printOption("3", "Edit characteristic");
+        MenuUtils.printOption("4", "Delete characteristic");
+        MenuUtils.printOption("0", "Back");
+        MenuUtils.printSeparator();
+        MenuUtils.printPrompt();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 0:
+                return MainMenuCommand.getInstance();
+            case 1:
+                return ViewCharacteristicCommand.getInstance();
+            case 2:
+                return AddCharacteristicCommand.getInstance();
+            case 3:
+                return new SelectCharacteristicCommand(new EditCharacteristicCommand());
+            case 4:
+                return new SelectCharacteristicCommand(new DeleteCharacteristicCommand());
+            default:
+                System.out.println("Unexcepted command!");
+                return this;
+        }
     }
 }
