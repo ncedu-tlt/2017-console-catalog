4b1912c9cd656872279b8ae65a07a413cd313612
Anton Kuzmin <Garthdex@gmail.com>
2016-11-16 03:03:52 +0400

    more cool print

--- src/main/java/ru/ncedu/menu/commands/characteristic/ViewCharacteristicCommand.java
+++ src/main/java/ru/ncedu/menu/commands/characteristic/ViewCharacteristicCommand.java
@@ -1,7 +1,11 @@
 package ru.ncedu.menu.commands.characteristic;
 
 import ru.ncedu.menu.commands.Command;
+import ru.ncedu.menu.models.Category;
 import ru.ncedu.menu.models.Characteristic;
+import ru.ncedu.menu.models.CharacteristicGroup;
+import ru.ncedu.menu.repositories.CategoriesRepository;
+import ru.ncedu.menu.repositories.CharacteristicGroupRepository;
 import ru.ncedu.menu.repositories.CharacteristicRepository;
 import ru.ncedu.menu.utils.MenuUtils;
 
@@ -9,6 +13,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ViewCharacteristicCommand implements Command {
+    List<Category> categories = CategoriesRepository.getInstance().get();
+    List<CharacteristicGroup> groups = CharacteristicGroupRepository.getInstance().get();
     private static ViewCharacteristicCommand instance;
 
     private ViewCharacteristicCommand() {}
@@ -30,8 +36,13 @@ public class ViewCharacteristicCommand implements Command {
             return CharacteristicMenuCommand.getInstance();
         }
 
+        MenuUtils.printCategorySeparator();
         for (Characteristic characteristic : characteristics) {
-            MenuUtils.printOption(String.valueOf(characteristic.getId()), characteristic.getName());
+            System.out.println("Characteristic ID: " + String.valueOf(characteristic.getId()) + "\n" +
+                    "Characteristic name: " + characteristic.getName() + "\n" +
+                    "Characteristic group: " + iteratorCharacteristicGroup(characteristic.getGroupId()).getName() + "\n"
+                    + "Category: " + iteratorCategory(characteristic.getCategoryId()).getName());
+            MenuUtils.printCategorySeparator();
         }
         MenuUtils.printSeparator();
 
@@ -41,5 +52,31 @@ public class ViewCharacteristicCommand implements Command {
         scanner.nextLine();
 
         return CharacteristicMenuCommand.getInstance();
+    }
+
+    /**
+     *
+     * iterator for print category name
+     * @return category
+     */
+    private Category iteratorCategory(long categoryId) {
+        for (Category category : categories) {
+            if (category.getId() == categoryId) return category;
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * iterator for print characteristic group name
+     * @return category
+     */
+    private CharacteristicGroup iteratorCharacteristicGroup(long characteristicGroupId) {
+        for (CharacteristicGroup group : groups) {
+            if (group.getId() == characteristicGroupId) return group;
+        }
+
+        return null;
     }
 }
