2754e3a8ca32b2972407a72c7b3fb0ce16a5a845
Rita Loginova <ritalog10@gmail.com>
2016-11-18 00:13:47 +0400

    change remove

--- src/main/java/ru/ncedu/menu/models/Market.java
+++ src/main/java/ru/ncedu/menu/models/Market.java
@@ -6,6 +6,7 @@ public class Market {
 
     private long id;
     private String name;
+    private long priceId;
 
     public Market() { }
 
@@ -37,5 +38,13 @@ public class Market {
     @XmlElement
     public void setName(String name) {
         this.name = name;
+    }
+
+    public long getPriceId() {
+        return priceId;
+    }
+    @XmlElement
+    public void setPriceId(long priceId) {
+        this.priceId = priceId;
     }
 }
